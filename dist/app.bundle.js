176bfdfb29ea15c34b9cf67959be394c1a381d2e
Neptune <alain@alains-iMac.local>
2018-04-28 13:29:52 +0800

    output extract file issue

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -6,9 +6,9 @@
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
@@ -33,9 +33,6 @@
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
-/******/ 	// identity function for calling harmony imports with the correct context
-/******/ 	__webpack_require__.i = function(value) { return value; };
-/******/
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
@@ -45,6 +42,11 @@
 /******/ 				get: getter
 /******/ 			});
 /******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 	};
 /******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -62,31 +64,33 @@
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
+/******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/app.js");
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
-/* unknown exports provided */
-/* all exports used */
-/*!**********************!*\
-  !*** ./src/app.scss ***!
-  \**********************/
-/***/ (function(module, exports) {
+/******/ ({
 
-eval("throw new Error(\"Module build failed: ReferenceError: window is not defined\\n    at eval (eval at <anonymous> (/Users/alain/Work/allURL/node_modules/style-loader/index.js!/Users/alain/Work/allURL/node_modules/css-loader/index.js!/Users/alain/Work/allURL/node_modules/sass-loader/lib/loader.js!/Users/alain/Work/allURL/src/app.scss:90:1), <anonymous>:23:2)\\n    at eval (eval at <anonymous> (/Users/alain/Work/allURL/node_modules/style-loader/index.js!/Users/alain/Work/allURL/node_modules/css-loader/index.js!/Users/alain/Work/allURL/node_modules/sass-loader/lib/loader.js!/Users/alain/Work/allURL/src/app.scss:90:1), <anonymous>:12:46)\\n    at module.exports (eval at <anonymous> (/Users/alain/Work/allURL/node_modules/style-loader/index.js!/Users/alain/Work/allURL/node_modules/css-loader/index.js!/Users/alain/Work/allURL/node_modules/sass-loader/lib/loader.js!/Users/alain/Work/allURL/src/app.scss:90:1), <anonymous>:77:88)\\n    at eval (eval at <anonymous> (/Users/alain/Work/allURL/node_modules/style-loader/index.js!/Users/alain/Work/allURL/node_modules/css-loader/index.js!/Users/alain/Work/allURL/node_modules/sass-loader/lib/loader.js!/Users/alain/Work/allURL/src/app.scss:123:1), <anonymous>:16:78)\\n    at Object.<anonymous> (/Users/alain/Work/allURL/node_modules/style-loader/index.js!/Users/alain/Work/allURL/node_modules/css-loader/index.js!/Users/alain/Work/allURL/node_modules/sass-loader/lib/loader.js!/Users/alain/Work/allURL/src/app.scss:123:1)\\n    at __webpack_require__ (/Users/alain/Work/allURL/node_modules/style-loader/index.js!/Users/alain/Work/allURL/node_modules/css-loader/index.js!/Users/alain/Work/allURL/node_modules/sass-loader/lib/loader.js!/Users/alain/Work/allURL/src/app.scss:21:30)\\n    at /Users/alain/Work/allURL/node_modules/style-loader/index.js!/Users/alain/Work/allURL/node_modules/css-loader/index.js!/Users/alain/Work/allURL/node_modules/sass-loader/lib/loader.js!/Users/alain/Work/allURL/src/app.scss:67:18\\n    at Object.<anonymous> (/Users/alain/Work/allURL/node_modules/style-loader/index.js!/Users/alain/Work/allURL/node_modules/css-loader/index.js!/Users/alain/Work/allURL/node_modules/sass-loader/lib/loader.js!/Users/alain/Work/allURL/src/app.scss:70:10)\\n    at Module._compile (module.js:643:30)\\n    at Object.exec (/Users/alain/Work/allURL/node_modules/webpack/lib/NormalModule.js:98:6)\\n    at Object.<anonymous> (/Users/alain/Work/allURL/node_modules/extract-text-webpack-plugin/loader.js:101:21)\\n    at Compiler.<anonymous> (/Users/alain/Work/allURL/node_modules/webpack/lib/Compiler.js:280:10)\\n    at /Users/alain/Work/allURL/node_modules/webpack/lib/Compiler.js:480:13\\n    at next (/Users/alain/Work/allURL/node_modules/webpack/node_modules/tapable/lib/Tapable.js:202:11)\\n    at Compiler.<anonymous> (/Users/alain/Work/allURL/node_modules/extract-text-webpack-plugin/loader.js:82:4)\\n    at next (/Users/alain/Work/allURL/node_modules/webpack/node_modules/tapable/lib/Tapable.js:204:14)\\n    at Compiler.<anonymous> (/Users/alain/Work/allURL/node_modules/webpack/lib/CachePlugin.js:62:5)\\n    at Compiler.applyPluginsAsyncSeries (/Users/alain/Work/allURL/node_modules/webpack/node_modules/tapable/lib/Tapable.js:206:13)\\n    at /Users/alain/Work/allURL/node_modules/webpack/lib/Compiler.js:477:10\\n    at Compilation.applyPluginsAsyncSeries (/Users/alain/Work/allURL/node_modules/webpack/node_modules/tapable/lib/Tapable.js:195:46)\\n    at self.applyPluginsAsync.err (/Users/alain/Work/allURL/node_modules/webpack/lib/Compilation.js:615:19)\\n    at Compilation.applyPluginsAsyncSeries (/Users/alain/Work/allURL/node_modules/webpack/node_modules/tapable/lib/Tapable.js:195:46)\\n    at self.applyPluginsAsync.err (/Users/alain/Work/allURL/node_modules/webpack/lib/Compilation.js:606:11)\\n    at Compilation.applyPluginsAsyncSeries (/Users/alain/Work/allURL/node_modules/webpack/node_modules/tapable/lib/Tapable.js:195:46)\\n    at self.applyPluginsAsync.err (/Users/alain/Work/allURL/node_modules/webpack/lib/Compilation.js:601:10)\\n    at Compilation.applyPluginsAsyncSeries (/Users/alain/Work/allURL/node_modules/webpack/node_modules/tapable/lib/Tapable.js:195:46)\\n    at sealPart2 (/Users/alain/Work/allURL/node_modules/webpack/lib/Compilation.js:597:9)\\n    at Compilation.applyPluginsAsyncSeries (/Users/alain/Work/allURL/node_modules/webpack/node_modules/tapable/lib/Tapable.js:195:46)\\n    at Compilation.seal (/Users/alain/Work/allURL/node_modules/webpack/lib/Compilation.js:545:8)\\n    at /Users/alain/Work/allURL/node_modules/webpack/lib/Compiler.js:474:16\");//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMC5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==");
-
-/***/ }),
-/* 1 */
-/* unknown exports provided */
-/* all exports used */
+/***/ "./src/app.js":
 /*!********************!*\
   !*** ./src/app.js ***!
   \********************/
+/*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("const css = __webpack_require__(/*! ./app.scss */ 0);\n\nconsole.log('apppppp.js again');\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiMS5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL3NyYy9hcHAuanM/N2FjOSJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBjc3MgPSByZXF1aXJlKCcuL2FwcC5zY3NzJyk7XG5cbmNvbnNvbGUubG9nKCdhcHBwcHBwLmpzIGFnYWluJyk7XG5cblxuXG4vLy8vLy8vLy8vLy8vLy8vLy9cbi8vIFdFQlBBQ0sgRk9PVEVSXG4vLyAuL3NyYy9hcHAuanNcbi8vIG1vZHVsZSBpZCA9IDFcbi8vIG1vZHVsZSBjaHVua3MgPSAwIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7Iiwic291cmNlUm9vdCI6IiJ9");
+eval("const css = __webpack_require__(/*! ./app.scss */ \"./src/app.scss\");\n\nconsole.log('apppppp.js again');\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvYXBwLmpzLmpzIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vc3JjL2FwcC5qcz8xMTEyIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IGNzcyA9IHJlcXVpcmUoJy4vYXBwLnNjc3MnKTtcblxuY29uc29sZS5sb2coJ2FwcHBwcHAuanMgYWdhaW4nKTtcbiJdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBOyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/app.js\n");
+
+/***/ }),
+
+/***/ "./src/app.scss":
+/*!**********************!*\
+  !*** ./src/app.scss ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvYXBwLnNjc3MuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvYXBwLnNjc3M/ZTZlMyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyByZW1vdmVkIGJ5IGV4dHJhY3QtdGV4dC13ZWJwYWNrLXBsdWdpbiJdLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/app.scss\n");
 
 /***/ })
-/******/ ]);
+
+/******/ });
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,36 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+//const path = require('dist');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: 'dist',
+    path: __dirname + '/dist',
     filename: 'app.bundle.js'
   },
+
   module: {
     rules:[
       //{test: /\.scss$/, use: ExtractTextPlugin.extract(['style-loader','css-loader','sass-loader'])}
-      { test: /\.scss$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: [ 'css-loader' ] }) }
+      //{ test: /\.scss$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: [ 'css-loader' ] }) }
+      //{ test: /\.scss$/, use: ExtractTextPlugin.extract(['style-loader','css-loader','sass-loader'])}
+
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //resolve-url-loader may be chained before sass-loader if necessary
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+
+
     ]
   },
+
   plugins: [
-    new ExtractTextPlugin({
-      filename: 'app.css',
-      disable: false,
-      allChunks: true
-    }),
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
+    new ExtractTextPlugin("app.css")
   ]
 
 }
